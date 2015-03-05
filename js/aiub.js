576f5d8ff6096848011ae3cfa540cf631f1f31bf
shemul hossain <shemulworld@gmail.com>
2015-03-06 00:14:13 +0600

    stable commit

--- js/aiub.js
+++ js/aiub.js
@@ -21,6 +21,7 @@ function loadNotice () {
 	    if(!feeds){
 	      // there was an error
 	     // console.log('errr');
+	      document.getElementById('aiub_notice').innerHTML = "Origin offline";
 	      return false;
 	    }
 	    // do whatever you want with feeds here
@@ -29,7 +30,7 @@ function loadNotice () {
 	      var entry = feeds.entries[i];
 	      //console.log(entry);
 	      // Entry title
-	      
+	      // console.log(entry);
 	      document.getElementById('aiub_notice').innerHTML += 
 	      "<a class='item item-icon-left' href='" + entry.link + "'><i class='icon ion-document-text'></i>" + entry.title.slice(0,30) + 
 	      "<p>"+entry.publishedDate.slice(0,26) + "</p></a>"			
